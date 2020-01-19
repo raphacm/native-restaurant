7cebf8fea3672910e32e646025395f918475b993
Raphael Costa <raphacm@gmail.com>
2020-01-18 23:40:16 -0300

    manage term state in SearchScreen component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+
+  const onTermSubmit = term => {
+    console.log(`Term: ${term} submited`);
+  };
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar
+        term={term}
+        onChangeTerm={setTerm}
+        onTermSubmit={onTermSubmit}
+      />
     </View>
   );
 };
