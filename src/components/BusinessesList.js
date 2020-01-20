deb2723f4dc8bb8c6d30e80152738615a216b323
Raphael Costa <raphacm@gmail.com>
2020-01-20 00:08:46 -0300

    include TouchableOpacity for each item of BusinessDetail and also include HOC withNavigation to get access to navigation properties

--- src/components/BusinessesList.js
+++ src/components/BusinessesList.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
+import { withNavigation } from "react-navigation";
 import BusinessDetail from "./BusinessDetail";
 
-const BusinessesList = ({ results = [], title }) => {
+const BusinessesList = ({ results, title, navigation }) => {
   return (
     <View style={styles.listContainer}>
       <Text style={styles.title}>{title}</Text>
@@ -11,7 +18,15 @@ const BusinessesList = ({ results = [], title }) => {
         showsHorizontalScrollIndicator={false}
         keyExtractor={results => results.id}
         data={results}
-        renderItem={({ item }) => <BusinessDetail {...item} />}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Detail", { id: item.id })}
+            >
+              <BusinessDetail {...item} />
+            </TouchableOpacity>
+          );
+        }}
       />
     </View>
   );
@@ -28,4 +43,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default BusinessesList;
+export default withNavigation(BusinessesList);
